fbe631299e346c845669598f6a946b12319fc7ec
Esther <devarsesther@gmail.com>
2025-09-05 13:01:53 +0200

    add file fichier csv et news et enregistremnent sur phpmyadmin

--- data/php-rigolo2.sql
+++ data/php-rigolo2.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `php-rigolo2`
+-- Database: `php_rigolo2`
 --
 
 -- --------------------------------------------------------
